16b68aebcc5290af7ba7527a69139e4e792e9883
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-09-12 19:28:22 +0800

    asas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 axios.defaults.withCredentials = true;
 
 useEffect(() => {
-  axios.get('http://localhost:3001/')
+  axios.get('https://iloilo-coffee-house-api.vercel.app/')
     .then(user => {
       setUser(user.data);
       console.log(user.data);
